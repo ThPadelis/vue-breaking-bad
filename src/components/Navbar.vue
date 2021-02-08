52361c5f2bd46d769d313a9ad620103e49df3626
ThPadelis <padelis.theodosiou@gmail.com>
2021-02-08 22:34:46 +0200

    Minor changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,15 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="info" sticky>
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  <b-navbar toggleable="lg" type="dark" variant="dark" sticky class="mb-4">
+    <b-navbar-brand href="#">VueBreakingBad</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <!-- <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>-->
-    </b-collapse>
+    <b-collapse id="nav-collapse" is-nav></b-collapse>
   </b-navbar>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "navbar",
+};
 </script>
-
-<style>
-</style>
